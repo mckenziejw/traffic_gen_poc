8fa47f9319e284f353314757ccacdb7f9dcb34c1
Joshua McKenzie <mckenzie.jw@gmail.com>
2024-06-12 14:10:59 +0200

    testing netplan build

--- build_netplan.py
+++ build_netplan.py
@@ -12,38 +12,38 @@ def main():
     client.connect('10.210.14.1', username='lab', password='lab123')
 
     stdin, stdout, stderr = client.exec_command('ls /sys/class/net')
-    wifis=[]
+    wifis_raw=[]
     for line in stdout:
-        wifis.append(line.strip('\n'))
-    print(wifis)
+        wifis_raw.append(line.strip('\n'))
+
     client.close()
+
+    environment = Environment(loader=FileSystemLoader("/home/lab/traffic_gen_poc/templates/"))
+    template = environment.get_template("main.tf.j2")
+
+    count=1
+    
+    data = {'wifis':[]}
+    for intf in data:
+        if re.match('wlp[a-zA-Z0-9]*', intf):
+            data['wifis'].append(
+                {
+                    'name':'wlan0',
+                    'dev_name': intf
+                }
+            )
+        elif re.match('wlx[a-zA-Z0-9]*', intf):
+            data['wifis'].append(
+                {
+                    'name':'wlan'+str(count),
+                    'dev_name': intf
+                }
+            )
+            count += 1
+
+    with open('/home/lab/traffic_gen_poc/main.tf', 'w') as out_file:
+        output = template.render(data)
+        out_file.write(output)
 
 if __name__ == "__main__":
     main()
-# environment = Environment(loader=FileSystemLoader("/home/lab/traffic_gen_poc/templates/"))
-# template = environment.get_template("main.tf.j2")
-
-# wifis = {'wifis':[]}
-
-# w_list = os.listdir('/sys/class/net/')
-# count=1
-# for intf in w_list:
-#     if re.match('wlp[a-zA-Z0-9]*', intf):
-#         wifis['wifis'].append(
-#             {
-#                 'name':'wlan0',
-#                 'dev_name': intf
-#             }
-#         )
-#     elif re.match('wlx[a-zA-Z0-9]*', intf):
-#         wifis['wifis'].append(
-#             {
-#                 'name':'wlan'+str(count),
-#                 'dev_name': intf
-#             }
-#         )
-#         count += 1
-
-# with open('/home/lab/traffic_gen_poc/main.tf', 'w') as out_file:
-#     output = template.render(wifis)
-#     out_file.write(output)
