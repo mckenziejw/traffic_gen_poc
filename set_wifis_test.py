497dfdaad5cfd875812bff4f512beb8756979231
Joshua McKenzie <mckenzie.jw@gmail.com>
2024-06-14 10:58:50 +0200

    testing netplan build

--- set_wifis_test.py
+++ set_wifis_test.py
@@ -11,35 +11,34 @@ client = Client(
     verify=False
 )
 
-parser = argparse.ArgumentParser(
-prog='MIST WiFi Controller',
-description='Update settings on WiFi Clients with self-test AP'
-)
+# parser = argparse.ArgumentParser(
+# prog='MIST WiFi Controller',
+# description='Update settings on WiFi Clients with self-test AP'
+# )
 
-parser.add_argument('-ssid')
-parser.add_argument('-psk')
+# parser.add_argument('-ssid')
+# parser.add_argument('-psk')
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
 client.authenticate('lab123')
 ## Provision the AP
 ap = client.instances.get('wifi-client-1')
-pprint(ap.state().network['eth1'])
+ssid = ap.state().network['eth1']['hwaddr'].replace(':','')
 
-# exit_code,s_out,s_err = ap.execute(
-#     commands = ['nmcli','device','eth1','hotspot','con-name','']
-# )
+exit_code,s_out,s_err = ap.execute(
+    commands = ['nmcli','device','eth1','hotspot','con-name',ssid, 'ssid', ssid, 'band','ac','password', 'lab123lab123']
+)
+print(exit_code,s_out,s_err)
 
-# containers = client.instances.all()
-# buffer = []
-# for c in containers:
-#     f = c.FilesManager(c)
-#     put_file = lambda data: f.put("/etc/wpa_supplicant/wpa_supplicant.conf",data)
-#     exit_code,s_out,s_err = c.execute(
-#         commands = ['wpa_passphrase', args.ssid, args.psk], stdout_handler=put_file
-#     )
-#     exit_code,s_out,s_err = c.execute(
-#         commands = ['wpa_supplicant','-B','-i','eth1','-c','/etc/wpa_supplicant/wpa_supplicant.conf']
-#     )
-#     print(exit_code,s_out,s_err)
+for i in range(2,5):
+    c = client.instances.get('wifi-client-{}'.format(i))
+    put_file = lambda data: f.put("/etc/wpa_supplicant/wpa_supplicant.conf",data)
+    exit_code,s_out,s_err = c.execute(
+        commands = ['wpa_passphrase', args.ssid, args.psk], stdout_handler=put_file
+    )
+    exit_code,s_out,s_err = c.execute(
+        commands = ['wpa_supplicant','-B','-i','eth1','-c','/etc/wpa_supplicant/wpa_supplicant.conf']
+    )
+    print(exit_code,s_out,s_err)
 
