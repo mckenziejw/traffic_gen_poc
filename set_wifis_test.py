73d123f6c81a41158afc1233b0a7931bb08d7df5
Joshua McKenzie <mckenzie.jw@gmail.com>
2024-06-14 11:34:02 +0200

    testing netplan build

--- set_wifis_test.py
+++ set_wifis_test.py
@@ -1,6 +1,7 @@
 from pylxd import Client
 from pylxd.models.instance import Instance
 from pprint import pprint
+import time
 
 import argparse
 
@@ -26,17 +27,21 @@ client.authenticate('lab123')
 ap = client.instances.get('wifi-client-1')
 ssid = ap.state().network['eth1']['hwaddr'].replace(':','')
 psk = 'lab123lab123'
-
+print("Installing AP dependencies")
 exit_code,s_out,s_err = ap.execute(
     commands = ['apt','install','-y','network-manager']
 )
 print(exit_code,s_out,s_err)
-
+print("Configuring AP hotspot with name {}".format(ssid))
 exit_code,s_out,s_err = ap.execute(
     commands = ['nmcli','device','wifi','hotspot','ifname','eth1','con-name',ssid, 'ssid', ssid, 'password', psk]
 )
 print(exit_code,s_out,s_err)
-
+if(exit_code == 0):
+    print("AP Successfully enabled")
+else:
+    print("AP initialization failed")
+time.sleep(10)
 for i in range(2,5):
     c = client.instances.get('wifi-client-{}'.format(i))
     f = c.FilesManager(c)
@@ -48,4 +53,10 @@ for i in range(2,5):
         commands = ['wpa_supplicant','-B','-i','eth1','-c','/etc/wpa_supplicant/wpa_supplicant.conf']
     )
     print(exit_code,s_out,s_err)
+    time.sleep(20)
+    if(c.state(),network['eth1']['address'] == ''):
+        exit_code,s_out,s_err = c.execute(
+        commands = ['dhclient','eth1']
+        )
+        print(exit_code,s_out,s_err)
 
