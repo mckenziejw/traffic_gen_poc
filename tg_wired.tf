0aedfc4eb02fb6db60a59bdf205af202be3f06ad
Joshua McKenzie <mckenzie.jw@gmail.com>
2024-06-20 15:25:39 +0200

    Adding terraform tg test

--- tg_wired.tf
+++ tg_wired.tf
@@ -38,3 +38,91 @@ resource "docker_network" "services_net_1" {
         ipvlan_mode = "l2"
     }
 }
+resource "docker_network" "services_net_2" {
+    provider = docker.client2
+    name = "services_net"
+    driver = "ipvlan"
+
+    ipam_config {
+        subnet = "10.42.0.0/24"
+        ip_range = "10.42.0.0/24"
+        gateway = "10.42.0.1"
+    }
+    options = {
+        parent = "eth0"
+        ipvlan_mode = "l2"
+    }
+}
+resource "docker_network" "services_net_3" {
+    provider = docker.client3
+    name = "services_net"
+    driver = "ipvlan"
+
+    ipam_config {
+        subnet = "10.43.0.0/24"
+        ip_range = "10.43.0.0/24"
+        gateway = "10.43.0.1"
+    }
+    options = {
+        parent = "eth0"
+        ipvlan_mode = "l2"
+    }
+}
+
+resource "docker_container" "web_client_1" {
+    provider = docker.client1
+    name = "web-client"
+    image = "autofunbot/tg_web_client"
+    hostname = "web-client-1"
+    host  {
+        host = "web-client-2"
+        ip = "10.42.0.2"
+    }
+    host {
+        host = "web-client-3"
+        ip = "10.43.0.2"
+    }
+
+    networks_advanced {
+        name = "services_net"
+        ipv4_address = "10.41.0.2"
+    }
+}
+resource "docker_container" "web_client_2" {
+    provider = docker.client2
+    name = "web-client"
+    image = "autofunbot/tg_web_client"
+    hostname = "web-client-2"
+    host  {
+        host = "web-client-1"
+        ip = "10.41.0.2"
+    }
+    host {
+        host = "web-client-3"
+        ip = "10.43.0.2"
+    }
+
+    networks_advanced {
+        name = "services_net"
+        ipv4_address = "10.42.0.2"
+    }
+}
+resource "docker_container" "web_client_3" {
+    provider = docker.client3
+    name = "web-client"
+    image = "autofunbot/tg_web_client"
+    hostname = "web-client-3"
+    host  {
+        host = "web-client-2"
+        ip = "10.42.0.2"
+    }
+    host {
+        host = "web-client-1"
+        ip = "10.41.0.2"
+    }
+
+    networks_advanced {
+        name = "services_net"
+        ipv4_address = "10.43.0.2"
+    }
+}
