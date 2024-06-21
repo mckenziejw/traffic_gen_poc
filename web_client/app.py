96d24521861b9ee8c8478872e3b2ad828c050104
Joshua McKenzie <mckenzie.jw@gmail.com>
2024-06-21 14:55:57 +0200

    Adding terraform tg test

--- web_client/app.py
+++ web_client/app.py
@@ -22,37 +22,20 @@ port = 1883
 topic = "web/{}".format(hostname)
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
-def connect_mqtt():
-    #def on_connect(client, userdata, flags, rc):
-    # For paho-mqtt 2.0.0, you need to add the properties parameter.
-    def on_connect(client, userdata, flags, rc, properties):
+def on_connect(client, userdata, flags, rc, properties):
         if rc == 0:
             print("Connected to MQTT Broker!")
+            client.subscribe(topic)
         else:
             print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
-    #client = mqtt_client.Client(client_id)
-
-    # For paho-mqtt 2.0.0, you need to set callback_api_version.
-    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
-
-    # client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(mqtt_broker, port)
-    return client
-
 def handle_mqtt_msg(client, userdata, msg):
     print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
-def subscribe(client: mqtt_client):
-    def on_message(client, userdata, msg):
+def on_message(client, userdata, msg):
         handle_mqtt_msg(client, userdata, msg)
-
-    client.subscribe(topic)
-    client.on_message = on_message
-
-client = connect_mqtt()
-subscribe(client)
+client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(mqtt_broker, port)
 client.loop_forever()
 
 # while True:
