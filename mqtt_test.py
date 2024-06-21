e4444f7957222f6583e210fd0b55d14b2b32d0e9
Joshua McKenzie <mckenzie.jw@gmail.com>
2024-06-21 14:05:03 +0200

    Adding terraform tg test

--- mqtt_test.py
+++ mqtt_test.py
@@ -10,18 +10,18 @@ topic = 'web/web-client-1'
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
+    #def on_connect(client, userdata, flags, rc):
     # For paho-mqtt 2.0.0, you need to add the properties parameter.
-    # def on_connect(client, userdata, flags, rc, properties):
+    def on_connect(client, userdata, flags, rc, properties):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
     # Set Connecting Client ID
-    client = mqtt_client.Client(client_id)
+    #client = mqtt_client.Client(client_id)
 
     # For paho-mqtt 2.0.0, you need to set callback_api_version.
-    # client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
 
     # client.username_pw_set(username, password)
     client.on_connect = on_connect
